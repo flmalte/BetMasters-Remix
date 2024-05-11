8a82d4952a03839d7dbbd28935c7ec5cfc91bcac
flmalte <62727638+flmalte@users.noreply.github.com>
2024-05-11 19:10:54 +0200

    League fetching

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,40 +1,52 @@
-import type { MetaFunction } from "@remix-run/node";
+import { MetaFunction } from "@remix-run/node";
+import { NavLink } from "@remix-run/react";
+import axios from "axios";
+import { useLoaderData } from "react-router";
 
 export const meta: MetaFunction = () => {
   return [
-    { title: "New Remix App" },
-    { name: "description", content: "Welcome to Remix!" },
+    { title: "BetMasters" },
+    { name: "description", content: "Welcome to BetMasters!" },
   ];
 };
 
+export async function loader() {
+  try {
+    const response = await axios.get(
+      "https://betmasters.azurewebsites.net/leagues/supported",
+    );
+    console.log(response.data);
+    return response.data;
+  } catch (error) {
+    console.error(error);
+    return error;
+  }
+}
+
 export default function Index() {
+  const data = useLoaderData<typeof loader>();
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
-      <h1 className="mx-auto p-5 text-green-600">Welcome to Remix</h1>
+      <h1 className="mx-auto p-5 text-green-600">Leages</h1>
       <ul>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/blog"
-            rel="noreferrer"
-          >
-            15m Quickstart Blog Tutorial
-          </a>
-        </li>
-        <li>
-          <a
-            target="_blank"
-            href="https://remix.run/tutorials/jokes"
-            rel="noreferrer"
-          >
-            Deep Dive Jokes App Tutorial
-          </a>
-        </li>
-        <li>
-          <a target="_blank" href="https://remix.run/docs" rel="noreferrer">
-            Remix Docs
-          </a>
-        </li>
+        {data.map((data) => (
+          <li key={data.name}>
+            <NavLink
+              to={`/league/${data.id}`}
+              prefetch="intent"
+              className={({ isActive, isPending }) =>
+                isPending
+                  ? "block rounded border-gray-700 px-3 py-2 text-black hover:bg-gray-700 hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-500"
+                  : isActive
+                    ? "block rounded bg-blue-700 px-3 py-2 text-black md:bg-transparent md:p-0 md:text-blue-500"
+                    : "block rounded border-gray-700 px-3 py-2 text-black hover:bg-gray-700 hover:text-white md:p-0 md:hover:bg-transparent md:hover:text-blue-500"
+              }
+            >
+              {data.name}
+            </NavLink>
+          </li>
+        ))}
       </ul>
     </div>
   );
